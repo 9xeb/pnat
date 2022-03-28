c77f3a31df5047a9093a14764e014c56a7fca499
9xeb <9xeb@example.com>
2022-03-28 18:49:16 +0200

    Drop duplicate entries with same timestamp and pid

--- tracker.py
+++ tracker.py
@@ -9,7 +9,7 @@ import threading
 active_connections = {}
 # I'm not sure a dictionary is thread safe when add/removes happen in parallel, so I rely on a simple lock
 active_connections_lock = threading.Lock()
-parsed_keys = ()
+#parsed_keys = ()
 
 # used to keep the two threads synchronized via conditions
 last_closed_timestamp = float(0.0)
@@ -28,8 +28,8 @@ def closed_tracker(condition):
 					print("Closed writer closed")
 					break
 				closesplit = closeline.split(' ')
-				closepid = closesplit[1]
-				closetime = closesplit[0]
+				closepid = int(closesplit[1])
+				closetime = int(closesplit[0].split('.')[0])
 				closeexe = closesplit[2].replace('\n','')
 
 				# Read connfile after we got a closeline, so we're sure that if the closed process did some network activity, we will find it in connfile
@@ -37,7 +37,7 @@ def closed_tracker(condition):
 					# get event keys with correct PID and that happened before the process died
 					# we cannot use a list generator because we need to get all the keys before popping
 					# a.k.a. we cannot pop in the dictionary while looping through it
-					keyslist = [key for key in active_connections.keys() if int(key[1]) == int(closepid) and float(key[0]) <= float(closetime)]
+					keyslist = [key for key in active_connections.keys() if key[1] == closepid and key[0] <= closetime]
 					for key in keyslist:
 						connline = active_connections.pop(key)
 						conntime = key[0]
@@ -57,7 +57,8 @@ def closed_tracker(condition):
 
 
 # For every new network event, grow a dictionary (timestamp,pid):(event metadata). This works as a producer.
-def conns_tracker(condition):
+# Unique parsed keys (used to avoid logging duplicate entries in the last second). This reduces log output by a lot.
+def conns_tracker(condition, parsed_keys={0}):
 	print("Started conns_tracker")
 	with open('/var/lib/pnat/pnat.log', 'a') as logfile:
 		with open('/var/lib/pnat/conn.fifo') as connfifo:
@@ -67,24 +68,30 @@ def conns_tracker(condition):
 					print("Conns writer closed")
 					break
 				connsplit = connline.split(' ')
-				connpid = connsplit[2]
-				conntime = connsplit[0]
+				connpid = int(connsplit[2])
+				conntime = int(connsplit[0].split('.')[0])
 				connip = connsplit[1]
 				connexe = connsplit[3].replace('\n','')
+
+				# forget more than 1 second old entries to avoid memory leaks
+				temp = {timestamp for timestamp in parsed_keys if conntime <= timestamp}
+				parsed_keys = temp
 
 				# Use (conntime, connpid) as dict key
 				with active_connections_lock:
 					# if new record is not in parsed keys of the current second
-					if (conntime, connpid) not in parsed_keys:
+					if conntime not in parsed_keys:
+						parsed_keys.add(conntime)
 						active_connections[(conntime, connpid)] = connline
-					print("[pending] exe={} ip={} start={}".format(connexe, connip, conntime))
-					logfile.write("[pending] exe={} ip={} start={}\n".format(connexe, connip, conntime))
-					logfile.flush()
+						print("[pending] exe={} ip={} start={}".format(connexe, connip, conntime))
+						logfile.write("[pending] exe={} ip={} start={}\n".format(connexe, connip, conntime))
+						logfile.flush()
+
 
 				# Update timestamp of the last processed record AFTER it's actually been processed
 				# Symmetrical to closed_tracker, however we notify here instead of waiting
 				with condition:
-					last_conn_timestamp = float(conntime)
+					last_conn_timestamp = conntime
 					if last_conn_timestamp > last_closed_timestamp:
 						#print("Conn notifying")
 						condition.notifyAll()
@@ -92,7 +99,7 @@ def conns_tracker(condition):
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
 	cv = threading.Condition()
-	futures = [executor.submit(conns_tracker, condition=cv), executor.submit(closed_tracker, condition=cv)]
+	futures = [executor.submit(conns_tracker, condition=cv, parsed_keys={0}), executor.submit(closed_tracker, condition=cv)]
 	for future in concurrent.futures.as_completed(futures):
 		try:
 			result = future.result()
