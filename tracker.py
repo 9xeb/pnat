3336e5007774d37051407df83f66b065ef10658d
9xeb <9xeb@example.com>
2022-03-30 02:09:56 +0200

    More jsons in the input/output flow

--- tracker.py
+++ tracker.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+# TODO: improve code quality using decorators to reuse most of the code
+
 import sys
 import re
 import concurrent.futures
 import threading
+import json
 
 # A dictionary to keep track of all network events to be processed
 active_connections = {}
@@ -17,7 +20,7 @@ last_conn_timestamp = float(0.0)
 
 # For every new exited process, find if the connection tracker found any network event related to it. Log everything that is found.
 # This consumes the dictionary produced by conns_tracker()
-def closed_tracker(condition):
+def closed_tracker(condition, parsed_keys={0}):
 	print("Started closed_tracker")
 	with open('/var/lib/pnat/pnat.log', 'a') as logfile:
 		# Resilient fifo follower
@@ -27,23 +30,26 @@ def closed_tracker(condition):
 				if len(closeline) == 0:
 					print("Closed writer closed")
 					break
-				closesplit = closeline.split(' ')
-				closepid = int(closesplit[1])
-				closetime = int(closesplit[0].split('.')[0])
-				closeexe = closesplit[2].replace('\n','')
+				closejson = json.loads(closeline)
+				closepid = int(closejson['pid'])
+				closetime = int(closejson['end'].split('.')[0])
+				closeexe = closejson['exe']
+
+				# TODO: use parsed_keys to make closed_tracker faster (skip duplicate entries in the same second)
 
 				# Read connfile after we got a closeline, so we're sure that if the closed process did some network activity, we will find it in connfile
 				with active_connections_lock:
 					# get event keys with correct PID and that happened before the process died
 					# we cannot use a list generator because we need to get all the keys before popping
 					# a.k.a. we cannot pop in the dictionary while looping through it
-					keyslist = [key for key in active_connections.keys() if key[1] == closepid and key[0] <= closetime]
-					for key in keyslist:
-						connline = active_connections.pop(key)
-						conntime = key[0]
-						connip = connline.split(' ')[1]
-						print("[closed] exe={} ip={} start={} end={}".format(closeexe, connip, conntime, closetime))
-						logfile.write("[closed] exe={} ip={} start={} end={}\n".format(closeexe, connip, conntime, closetime))
+					keyslist = [key for key in active_connections.keys() if key[2] == closepid and key[0] <= closetime]
+					for key in keyslist:	# for each (start,ip,pid) tuple, with the same pid as closepid and older start timestamp than closetime
+						connjson = active_connections.pop(key)
+						conntime = connjson['start']	# already made to integer by conns_tracker
+						connip = connjson['ip']
+						close_json = {'type': 'closed', 'start': conntime, 'end': closetime, 'ip': connip, 'exe': closeexe}
+						print("{}".format(close_json))
+						logfile.write("{}\n".format(close_json))
 					logfile.flush()
 
 				# smart synchronization with Condition
@@ -58,7 +64,7 @@ def closed_tracker(condition):
 
 # For every new network event, grow a dictionary (timestamp,pid):(event metadata). This works as a producer.
 # Unique parsed keys (used to avoid logging duplicate entries in the last second). This reduces log output by a lot.
-def conns_tracker(condition, parsed_keys={0}):
+def conns_tracker(condition, parsed_keys={(0,'',0)}):
 	print("Started conns_tracker")
 	with open('/var/lib/pnat/pnat.log', 'a') as logfile:
 		with open('/var/lib/pnat/conn.fifo') as connfifo:
@@ -67,26 +73,30 @@ def conns_tracker(condition, parsed_keys={0}):
 				if len(connline) == 0:
 					print("Conns writer closed")
 					break
-				connsplit = connline.split(' ')
-				connpid = int(connsplit[2])
-				conntime = int(connsplit[0].split('.')[0])
-				connip = connsplit[1]
-				connexe = connsplit[3].replace('\n','')
+				connjson = json.loads(connline)
+				#print("[pending] {}".format(connjson))
+				# Parse keys from json
+				connpid = int(connjson['pid'])
+				conntime = int(connjson['start'].split('.')[0])
+				connips = [ connjson[key] for key in connjson.keys() if key.startswith("ip") ]	# all values with keys that start with "ip"
+				connexe = connjson['exe']
 
-				# forget more than 1 second old entries to avoid memory leaks
-				temp = {timestamp for timestamp in parsed_keys if conntime <= timestamp}
+				# forget entries that are more than 1 second old to avoid memory leaks
+				temp = {key for key in parsed_keys if conntime <= key[0]}
 				parsed_keys = temp
 
-				# Use (conntime, connpid) as dict key
+				# Do some checks to limit redundancies in the same second, then package a new json
+				# Use (conntime, connip) as dict key
 				with active_connections_lock:
-					# if new record is not in parsed keys of the current second
-					if conntime not in parsed_keys:
-						parsed_keys.add(conntime)
-						active_connections[(conntime, connpid)] = connline
-						print("[pending] exe={} ip={} start={}".format(connexe, connip, conntime))
-						logfile.write("[pending] exe={} ip={} start={}\n".format(connexe, connip, conntime))
-						logfile.flush()
-
+					for connip in connips:
+						# if new record is not in parsed keys of the current (second,ip,pid)
+						if (conntime,connip,connpid) not in parsed_keys:
+							parsed_keys.add((conntime,connip,connpid))
+							stripped_connjson = {'type':'pending', 'start': conntime, 'ip': connip, 'exe': connexe}
+							active_connections[(conntime, connip, connpid)] = stripped_connjson
+							print("{}".format(stripped_connjson))
+							logfile.write("{}\n".format(stripped_connjson))
+							logfile.flush()
 
 				# Update timestamp of the last processed record AFTER it's actually been processed
 				# Symmetrical to closed_tracker, however we notify here instead of waiting
@@ -99,7 +109,7 @@ def conns_tracker(condition, parsed_keys={0}):
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
 	cv = threading.Condition()
-	futures = [executor.submit(conns_tracker, condition=cv, parsed_keys={0}), executor.submit(closed_tracker, condition=cv)]
+	futures = [executor.submit(conns_tracker, condition=cv, parsed_keys={(0,'',0)}), executor.submit(closed_tracker, condition=cv)]
 	for future in concurrent.futures.as_completed(futures):
 		try:
 			result = future.result()
