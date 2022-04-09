0c3da6e68f28674dd757e6766a943fc69d270420
9xeb <9xeb@example.com>
2022-04-09 03:08:07 +0200

    Remove useless scripts

--- setup_auditd_remote.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# the following script sets an auditd collector
-# in order to keep logs in a secure location for further analysis
-apt update && apt install auditd audispd-remote sqlite3
-sed -i 's/##tcp_listen_port =.*/tcp_listen_port = 60/g' /etc/audit/auditd.conf
-
